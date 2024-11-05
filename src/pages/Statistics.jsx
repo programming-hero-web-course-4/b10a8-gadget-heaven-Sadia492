5fb204e6b607d7f4588a046191159317219bf575
Sadia492 <sadiaafrin2792>
2024-11-05 20:13:11 +0600

    statistics updated and added a composed chart

--- src/pages/Statistics.jsx
+++ src/pages/Statistics.jsx
@@ -1,5 +1,41 @@
 import React from "react";
+import { useLoaderData } from "react-router-dom";
+import {
+  ComposedChart,
+  Area,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  Scatter,
+  ResponsiveContainer,
+} from "recharts";
 
 export default function Statistics() {
-  return <div>I am Statistics</div>;
+  const products = useLoaderData();
+  return (
+    <ResponsiveContainer width="100%" height={400}>
+      <ComposedChart
+        data={products}
+        margin={{
+          top: 20,
+          right: 20,
+          bottom: 20,
+          left: 20,
+        }}
+      >
+        <CartesianGrid stroke="#f5f5f5" />
+        <XAxis dataKey="product_title" scale="band" />
+        <YAxis dataKey="price" />
+        <Tooltip />
+        <Legend />
+        <Area type="monotone" dataKey="price" fill="#8884d8" stroke="#8884d8" />
+        <Bar dataKey="price" barSize={20} fill="#413ea0" />
+        {/* <Line type="monotone" dataKey="uv" stroke="#ff7300" /> */}
+        <Scatter dataKey="rating" fill="red" />
+      </ComposedChart>
+    </ResponsiveContainer>
+  );
 }
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -40,6 +40,7 @@ const router = createBrowserRouter([
       {
         path: "/statistics",
         element: <Statistics></Statistics>,
+        loader: () => fetch("../products.json"),
       },
       {
         path: "/dashboard",
